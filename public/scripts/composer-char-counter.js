84f0204c4bba2451549ebbe7e7d90dc28cba245d
Margaryta <margaryta.lanova@gmail.com>
2022-10-12 14:34:41 -0700

    created the HTML markup and CSS styling for the tweet element

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
   const maxLength = 140;
   let num = maxLength;
@@ -16,3 +17,22 @@ $(document).ready(function() {
     }
   });
 });
+
+// $(document).ready(function() {
+//   console.log(this)
+//   const maxLength = 140; //max value 
+//   let num = maxLength;
+
+// $("#tweet-text").on( "keydown", function( event ) {
+//     // const val = $( "#tweet-text" ).val();
+//     const tweetLength = $('tweet-text').val().length
+//     num = maxLength - tweetLength;
+//     if(num < 0) {
+//       $(this).siblings('div').children('output').val(num).css('color', 'red')
+//     } else {
+//       /* assign counter with num */
+//       $(this).siblings('div').children('output').val(num).css('color', 'grey')
+//     }
+// });
+
+// });
