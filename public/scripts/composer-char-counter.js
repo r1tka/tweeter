86862dd0eb0a0af84b46ec7ade6d22de6b8b5286
Margaryta <margaryta.lanova@gmail.com>
2022-10-12 09:47:03 -0700

    added a composer-char-counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  const maxLength = 140;
+  let num = maxLength;
+/* set the max value of 140 */
+  $('.new-tweet div output').val(num);
+/* decrease num after every keydown */
+  $('#tweet-text').keydown(function () {
+    const tweetLength = $('#tweet-text').val().length;
+    num = maxLength - tweetLength;
+    if (num < 0) {
+      /* change color to red when negative */
+      $(this).siblings('div').children('output').val(num).css('color', 'red')
+    } else {
+      /* assign counter with num */
+      $(this).siblings('div').children('output').val(num).css('color', 'grey')
+    }
+  });
+});
