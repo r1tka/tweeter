452af0ef9b0bf79c65f2ce8db5153ae10537c000
Margaryta <margaryta.lanova@gmail.com>
2022-11-09 14:48:12 -0800

    downloaded timeago library

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,12 @@
-const parseDate = function(time) {
-  const timeDifference = Date.parse(new Date) - time
-  const sec = timeDifference / 1000;
-  const min = sec / 60;
-  const hour = min / 60;
-  const day = Math.floor(hour / 24);
-  const year = Math.floor(day / 365);
-  if(year) {
-    return `${year} years ago`
-} else if (day) {
-  return `${day} days ago`
-} else if (hour) {
-  return `${hour} hours ago`
-} else if (min) {
-  return `${min} minutes ago`
-} else {
-  return `${sec} seconds ago`
-};
-  
-};
 /* render insecure text: */
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
-const createTweetElement = function(data) {
-  return ( `<section class="tweet">
+const createTweetElement = function (data) {
+  const ago = timeago.format(data.created_at);
+  return `<section class="tweet">
   <header class="tweet-header">
     <div class="avatar-container">
       <img class='avatar' src=${data.user.avatars}>
@@ -39,59 +20,56 @@ const createTweetElement = function(data) {
     </p>
   </div>
   <footer class="tweet-footer">
-    <p class="tweet-date">${parseDate(data.created_at)}</p>
+    <p class="tweet-date">${ago}</p>
     <div class="tweet-icon-buttons">
       <i class="fa-thin fa-flag"></i>
       <i class="fa-thin fa-retweet"></i>
       <i class="fa-thin fa-heart"></i>
     </div>
   </footer>      
-</section>`
-)}
- 
-const renderTweets = function(tweets) {
-  for(let tweet of tweets) {
-    $(`.tweets-container`).prepend(createTweetElement(tweet))
-  }}
+</section>`;
+};
+
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    $(`.tweets-container`).prepend(createTweetElement(tweet));
+  }
+};
 
 $(document).ready(() => {
-
-  
-  $( ".new-tweet-form" ).submit(function( event ) {
+  $(".new-tweet-form").submit(function (event) {
     event.preventDefault();
-    const cleanData = $("#tweet-text").serialize()
+    const cleanData = $("#tweet-text").serialize();
     if (cleanData.length > 145) {
-      $('.error').text(`❌ Tweet content is too long!`);
-      $('.error').css('border', '4px solid red');
-      $('.error').slideDown(1000);
+      $(".error").text(`❗️ Tweet content is too long!❗️`);
+      $(".error").css("border", "4px solid red");
+      $(".error").slideDown(1000);
     } else if (cleanData.length === 5) {
-      $('.error').text(`❌ Tweet can not be empty!`);
-      $('.error').css('border', '4px solid red');
-      $('.error').slideDown(1000);
+      $(".error").text(`❗️ Tweet can not be empty!❗️`);
+      $(".error").css("border", "2px solid red");
+      $(".error").slideDown(1000);
     } else {
-      $('.error').slideUp(1000);
+      $(".error").slideUp(1000);
       $.ajax({
-        type: 'POST',
+        type: "POST",
         url: "/tweets",
-        data: cleanData
-      })
-      .then(() => {
+        data: cleanData,
+      }).then(() => {
         loadtweets();
       });
     }
-    
-    });
+  });
 });
-const loadtweets = function() {
+const loadtweets = function () {
   $.ajax({
-    type: 'GET',
+    type: "GET",
     url: "/tweets",
-  }).then((data) => {
-    renderTweets(data)
-    console.log('data', data)
-  }).catch((error) => {
-    console.log('error', error)
   })
-}
-
-
+    .then((data) => {
+      renderTweets(data);
+      console.log("data", data);
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+};
